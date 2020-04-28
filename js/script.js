60a39e099f60f4adeb3fa8319c5fc5d673bda29f
GracielaCampis <animallover4278@gmail.com>
2020-04-28 22:35:40 +0000

    API stuffy stuff

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var request_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
+$("#search-button").click(function() {
+  fetch(request_url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
 
-$("#search-button").click(function(){
-  
-  
-  
+        console.log(data.url)
+    })
 });
 
